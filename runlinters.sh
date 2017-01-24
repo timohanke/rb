905f5b6cf9718ecd94e093b66c9471b259a60568
JW Bell <bjwbell@gmail.com>
2017-01-23 20:57:27 -0800

    Remove TTY option from docker run

--- runlinters.sh
+++ runlinters.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 # run linters
-docker run --rm -it -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/bls/...
+docker run --rm -i -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/bls/...
 if [ $? -ne 0 ];
 then
     echo "failed gometalinter"
     exit 1
 fi
 echo "OK for gometalinter bls"
-docker run --rm -it -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/blscgo/...
+docker run --rm -i -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/blscgo/...
 if [ $? -ne 0 ];
 then
     echo "failed gometalinter"
@@ -16,7 +16,7 @@ then
 fi
 echo "OK for gometalinter blscgo"
 
-docker run --rm -it -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/common2/...
+docker run --rm -i -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/common2/...
 if [ $? -ne 0 ];
 then
     echo "failed gometalinter"
@@ -24,7 +24,7 @@ then
 fi
 echo "OK for gometalinter common2"
 
-docker run --rm -it -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/sim/...
+docker run --rm -i -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/sim/...
 if [ $? -ne 0 ];
 then
     echo "failed gometalinter sim"
@@ -32,7 +32,7 @@ then
 fi
 echo "OK for gometalinter sim"
 
-docker run --rm -it -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/state/...
+docker run --rm -i -v `pwd`/go:/go dfinity/build-env gometalinter --deadline=10s --disable=gas --disable=gocyclo --disable=gotype --disable=interfacer src/state/...
 if [ $? -ne 0 ];
 then
     echo "failed gometalinter"
